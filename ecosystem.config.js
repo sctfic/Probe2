fed9d6d6cd816002ab3472717159376df9a9155e
Alban <alban.lopez@gmail.com>
2025-06-23 00:07:12 +0200

    first file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = { // Configuration de PM2
+    apps: [
+      {
+        name: 'Probe2', //  Therapie Facturation
+        script: 'node app.js',          // Le fichier principal de ton application
+        watch: true,                      // Active la surveillance des fichiers
+        ignore_watch: ['node_modules', 'logs', 'data', 'docs','.git'],  // Liste des fichiers/répertoires à ignorer
+        env: {
+          NODE_ENV: 'development',        // Définir l'environnement pour le développement
+          PORT: 2222,
+        },
+        env_production: {
+          NODE_ENV: 'production',         // Environnement de production
+          PORT: 2222,
+        },
+      },
+    ],
+  };
